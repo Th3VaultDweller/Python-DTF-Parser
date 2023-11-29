50f633c5d69a1baf37ec06e0a73625af10839d6c
Th3VaultDweller <aztec242@yandex.ru>
2023-11-29 22:35:46 +0300

    Added code for likes and post views

--- dtf_parser.py
+++ dtf_parser.py
@@ -11,8 +11,17 @@ links = soup.find_all("a", class_="content-link")
 
 # вывод названия книги и url-ссылки
 for i, link in enumerate(links):
-    url = link.get("href")  # ссылка
     # name = link.text()  # название ссылки
+    url = link.get("href")  # ссылка
+    post_viewers_count = link.find_all(
+        "span", class_="post-counters__item"
+    )  # кол-во просмотров поста
+    likes_count = link.find_all(
+        "span", class_="like-button__count"
+    )  # кол-во лайков поста
+
     print(i)
     # print(f"Name: {name}")
     print(f"URL: {url}")
+    print(f"Number of views: {post_viewers_count}")
+    print(f"Number of likes: {likes_count}")
