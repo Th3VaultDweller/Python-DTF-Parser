4d26c7375f6c3a2b931567c390f253fc5ac81f2f
Th3VaultDweller <aztec242@yandex.ru>
2023-11-29 22:15:37 +0300

    Changed the site name

--- dtf_parser.py
+++ dtf_parser.py
@@ -2,17 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 # запрос GET на получение html-кода страницы
-html = requests.get("https://www.wizardingworld.com/discover/books").text
+html = requests.get("https://dtf.ru/").text
 
 soup = BeautifulSoup(html, "lxml")
 
 # запрос GET на получение всех ссылок на книги
-links = soup.find_all("a", class_="ProductCard_link__z-ZoA")
+links = soup.find_all("a", class_="content-link")
 
 # вывод названия книги и url-ссылки
 for i, link in enumerate(links):
     url = link.get("href")  # ссылка
-    name = link.text()  # название ссылки
+    # name = link.text()  # название ссылки
     print(i)
-    print(f"Name: {name}")
+    # print(f"Name: {name}")
     print(f"URL: {url}")
