ddf2964d03ae7ee525e9aeefdcd719ee11bb723e
Th3VaultDweller <aztec242@yandex.ru>
2023-11-30 23:01:20 +0300

    Minor tweaks

--- dtf_parser.py
+++ dtf_parser.py
@@ -6,19 +6,19 @@ html = requests.get("https://dtf.ru/").text
 
 soup = BeautifulSoup(html, "lxml")
 
-# запрос GET на получение всех ссылок на книги
+# запрос GET на получение всех ссылок на посты
 links = soup.find_all("a", class_="content-link")
 
 # вывод названия поста и url-ссылки
 for i, link in enumerate(links):
     # name = link.text()  # название ссылки
     url = link.get("href")  # ссылка
-    post_viewers_count = soup.find_all(
+    post_viewers_count = link.find_all(
         "div", class_="post-counters__item"
-    ).text()  # кол-во просмотров поста
-    likes_count = soup.find_all(
+    )  # кол-во просмотров поста
+    likes_count = link.find_all(
         "button", class_="like-button like-button--default like-button--action-like"
-    ).text()  # кол-во лайков поста
+    )  # кол-во лайков поста
 
     print(i)
     # print(f"Name: {name}")
